4ff72c7775b22c504a970d9d2723a374833ca095
Kelly <williamgunawaneka@gmail.com>
2019-11-06 23:52:28 +0700

    styled home page

--- Bjora/resources/views/questions.blade.php
+++ Bjora/resources/views/questions.blade.php
@@ -3,33 +3,46 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Home</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 @component('parts.statusMessage')@endcomponent
+                    <div class="form-group col-form-label">
                     @component('parts.fastFormTemplate', ['action' => 'home', 'method' => 'GET', 'button' => 'Search'])
-                        <input type='text' name='search' value="{{ $search??"" }}">
+                        <input type='text' width="200" name='search' value="{{ $search??"" }}">
                     @endcomponent
-                    {{-- <ul> --}}
-                        @foreach ($question as $q)
-                            {{-- <li> --}}
-                                    {{-- <div class="col-md-6 text-md-right"> --}}
-                                <div class="col-md-10">
+                    </div>
+                    @foreach ($question as $q)
+                        <div class="col-mid-8" style="margin-bottom: 20px">
+                            
+                            <div class="col-mid-10">
+                                {{ $q->topic }}<br>
                                 <a href="/questions/{{$q->id}}">{{$q->question}}</a>
-                                {{ $q->topic }}
-                                
-                                    {{-- </div> --}}
-                                </div>
-                            {{-- </li> --}}
-                        @endforeach
-                    {{-- </ul> --}}
+                            </div>
+                                {{-- <span class="text-md-right"> --}}
+                            <div class="col-mid-10" style="margin-bottom: 90px">
+                                <span class="col-form-label text-md-left" style="float: left;">
+                                    @component('parts.PP', ['user' => $q->owner, 'size' => '70px', 'radius' => '100%'])@endcomponent
+                                </span>
+                                <span class="col-md-4 col-form-label text-md-left" style="float: left;">
+                                        <a class="nav-item" href="/users/{{ $q->owner }}">{{ $q->owner->name }}</a>
+                                        <br>
+                                        Created at: {{ $q->created_at }}
+                                </span>
+                            </div>
+                            {{ $q->question }}
+                            <br>
+                            <a class="btn btn-primary" href="/questions/{{ $q->id }}">Answer</a>
+                            {{-- <div class="text-md-left" style="text-align: right">{{ $q->created_at }}</div> --}}
+                            {{-- <span class="text-md-right" style="float: right"> --}}
+                                {{-- {{ $q->topic }} --}}
+                            {{-- </span> --}}
+                                {{-- </span> --}}
+                            {{-- </span> --}}
+                        </div>
+                    @endforeach
 
                     {{-- {{ $question->links() }} <- Search tidak persistent --}}
                     {{ $question->appends(request()->except('page'))->links() }} {{-- <- Pake ini supaya search bisa persistent waktu paginate --}}
