0a8c02ac3eb108b6aff20db27417df4c2cdc086f
Kelly <williamgunawaneka@gmail.com>
2019-11-14 21:43:29 +0700

    fixed app name, fixed inbox page header

--- Bjora/resources/views/messages.blade.php
+++ Bjora/resources/views/messages.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-10">
             <div class="card">
-                <div class="card-header">Profiles</div>
+                <div class="card-header">Inbox</div>
 
                 <div class="card-body">
 @component('parts.statusMessage')@endcomponent
